690d021f36ff84828fb6587c422c2dda96297bc3
park.suhyuk <park.suhyuk@gmail.com>
2018-03-10 00:20:06 +0900

    add thread pool testing

---
+++ src/main/java/com/ncsoft/dataplatform/dummies/Runner.java
@@ -0,0 +1,36 @@
+package com.ncsoft.dataplatform.dummies;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ *
+ * @author John Yeary
+ * @version 1.0
+ */
+public class Runner implements Runnable {
+
+	public static int count;
+	private final CountDownLatch finished;
+	private final CountDownLatch start;
+
+	public Runner(CountDownLatch start, CountDownLatch finished) {
+		this.start = start;
+		this.finished = finished;
+	}
+
+	@Override
+	public void run() {
+		try {
+			start.await();
+			add(1);
+			finished.countDown();
+		} catch (InterruptedException ex) {
+		} // return;
+
+	}
+
+	public static synchronized void add(int value) {
+		count = count + value;
+		System.out.println("runner count: " + count);
+	}
+}
