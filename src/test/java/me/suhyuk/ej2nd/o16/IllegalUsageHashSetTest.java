435e57c360e33c52bda323859daf75f7b9975d41
psyoblade <park.suhyuk@gmail.com>
2020-02-25 00:04:03 +0900

    [구현] 상속 대신 컴포지션을 사용하자

--- src/test/java/me/suhyuk/ej2nd/o16/IllegalUsageHashSetTest.java
+++ src/test/java/me/suhyuk/ej2nd/o16/IllegalUsageHashSetTest.java
@@ -3,6 +3,7 @@ package me.suhyuk.ej2nd.o16;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,5 +14,12 @@ public class IllegalUsageHashSetTest {
         IllegalUsageHashSet<String> set = new IllegalUsageHashSet<>();
         set.addAll(Arrays.asList("one", "two", "three"));
         assertEquals(6, set.getAddedCount());
+    }
+
+    @Test
+    public void 컴포지션을_이용한_셋클래스_예제() {
+        InstrumentedHashSet<String> set = new InstrumentedHashSet<>(new HashSet<String>());
+        set.addAll(Arrays.asList("one", "two", "three"));
+        assertEquals(3, set.getAddedCount());
     }
 }
